76fcd8d9f8af123b3a1a752d525cbe7013f2e3ed
Hoang Minh <ranhroi10@gmail.com>
2020-11-16 09:32:00 +0700

    add validPhoneNumber

--- Assignment_CMS/src/Validate.java
+++ Assignment_CMS/src/Validate.java
@@ -1,15 +1,29 @@
 
 public class Validate {
-   private boolean verifyEmail(String email)
-{
-    email = email.trim();
 
-    if(email == null || email.equals(""))
-        return false;
+    private boolean verifyEmail(String email) {
+        email = email.trim();
 
-    if(!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"))
-        return false;
+        if (email == null || email.equals("")) {
+            return false;
+        }
 
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean verifyPhoneNumber(String phoneNumber) {
+        phoneNumber = phoneNumber.trim();
+        if (phoneNumber == null || phoneNumber.equals("")) {
+            return false;
+        }
+        if (!phoneNumber.matches("+084-0-9")){
+            return false;
+    }
     return true;
-} 
 }
+}
+
